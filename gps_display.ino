a9ec304ccb7d462356009e26c94ebb9bfc37b3e9
Christopher Agocs <chris@agocs.org>
2023-06-12 20:16:23 -0700

    Add some basic code for the gps display

---
+++ gps_display.ino
@@ -0,0 +1,171 @@
+#include <Adafruit_GrayOLED.h>
+#include <gfxfont.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SPITFT.h>
+#include <Adafruit_SPITFT_Macros.h>
+
+#include <splash.h>
+#include <Adafruit_SH110X.h>
+
+#include <Adafruit_GPS.h>
+
+// what's the name of the hardware serial port?
+#define GPSSerial Serial1
+
+// Connect to the GPS on the hardware port
+Adafruit_GPS GPS(&GPSSerial);
+
+// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
+// Set to 'true' if you want to debug and listen to the raw GPS sentences
+#define GPSECHO false
+
+uint32_t timer = millis();
+
+
+Adafruit_SH1107 display = Adafruit_SH1107(64, 128, &Wire);
+
+// OLED FeatherWing buttons map to different pins depending on board:
+#if defined(ESP8266)
+  #define BUTTON_A  0
+  #define BUTTON_B 16
+  #define BUTTON_C  2
+#elif defined(ESP32) && !defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2)
+  #define BUTTON_A 15
+  #define BUTTON_B 32
+  #define BUTTON_C 14
+#elif defined(ARDUINO_STM32_FEATHER)
+  #define BUTTON_A PA15
+  #define BUTTON_B PC7
+  #define BUTTON_C PC5
+#elif defined(TEENSYDUINO)
+  #define BUTTON_A  4
+  #define BUTTON_B  3
+  #define BUTTON_C  8
+#elif defined(ARDUINO_NRF52832_FEATHER)
+  #define BUTTON_A 31
+  #define BUTTON_B 30
+  #define BUTTON_C 27
+#else // 32u4, M0, M4, nrf52840, esp32-s2 and 328p
+  #define BUTTON_A  9
+  #define BUTTON_B  6
+  #define BUTTON_C  5
+#endif
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println("128x64 OLED FeatherWing test");
+  delay(250); // wait for the OLED to power up
+  display.begin(0x3C, true); // Address 0x3C default
+
+  Serial.println("OLED begun");
+
+  // Show image buffer on the display hardware.
+  // Since the buffer is intialized with an Adafruit splashscreen
+  // internally, this will display the splashscreen.
+  display.display();
+  delay(1000);
+
+  // Clear the buffer.
+  display.clearDisplay();
+  display.display();
+
+  display.setRotation(1);
+  Serial.println("Button test");
+
+  pinMode(BUTTON_A, INPUT_PULLUP);
+  pinMode(BUTTON_B, INPUT_PULLUP);
+  pinMode(BUTTON_C, INPUT_PULLUP);
+
+  // text display tests
+  display.setTextSize(1);
+  display.setTextColor(SH110X_WHITE);
+  display.setCursor(0,0);
+  display.println("Setting up GPS");
+  display.display(); // actually display all of the above
+
+  
+  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+  GPS.begin(9600);
+  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  // uncomment this line to turn on only the "minimum recommended" data
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
+  // the parser doesn't care about other sentences at this time
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+  // For the parsing code to work nicely and have time to sort thru the data, and
+  // print it out we don't suggest using anything higher than 1 Hz
+
+  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+  delay(1000);
+
+  // Ask for firmware version
+  GPSSerial.println(PMTK_Q_RELEASE);
+  
+}
+
+void loop() {
+
+  // read data from the GPS in the 'main loop'
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+  if (GPSECHO)
+    if (c) Serial.print(c);
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences!
+    // so be very wary if using OUTPUT_ALLDATA and trying to print out data
+    Serial.print(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
+    if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
+      return; // we can fail to parse a sentence in which case we should just wait for another
+  }
+
+  // approximately every 2 seconds or so, print out the current stats
+  if (millis() - timer > 2000) {
+    timer = millis(); // reset the timer
+    display.clearDisplay();
+    display.print("\nTime: ");
+    if (GPS.hour < 10) { display.print('0'); }
+    display.print(GPS.hour, DEC); display.print(':');
+    if (GPS.minute < 10) { display.print('0'); }
+    display.print(GPS.minute, DEC); display.print(':');
+    if (GPS.seconds < 10) { display.print('0'); }
+    display.print(GPS.seconds, DEC); display.print('.');
+    if (GPS.milliseconds < 10) {
+      display.print("00");
+    } else if (GPS.milliseconds > 9 && GPS.milliseconds < 100) {
+      display.print("0");
+    }
+    display.println(GPS.milliseconds);
+    display.print("Date: ");
+    display.print(GPS.day, DEC); display.print('/');
+    display.print(GPS.month, DEC); display.print("/20");
+    display.println(GPS.year, DEC);
+    display.print("Fix: "); display.print((int)GPS.fix);
+    display.print(" quality: "); display.println((int)GPS.fixquality);
+    if (GPS.fix) {
+      display.print("Location: ");
+      display.print(GPS.latitude, 4); display.print(GPS.lat);
+      display.print(", ");
+      display.print(GPS.longitude, 4); display.println(GPS.lon);
+      display.print("Speed (knots): "); display.println(GPS.speed);
+      display.print("Angle: "); display.println(GPS.angle);
+      display.print("Altitude: "); display.println(GPS.altitude);
+      display.print("Satellites: "); display.println((int)GPS.satellites);
+      display.print("Antenna status: "); display.println((int)GPS.antenna);
+    }
+  }
+
+  
+  if(!digitalRead(BUTTON_A)) display.print("A");
+  if(!digitalRead(BUTTON_B)) display.print("B");
+  if(!digitalRead(BUTTON_C)) display.print("C");
+  delay(10);
+  yield();
+  display.display();
+}
